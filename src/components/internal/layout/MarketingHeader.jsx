b4a598246042c6088b047ac48a01cb3f9e3f7c93
harshit-parasahr-3 <parasharharshit99@gmail.com>
2025-01-05 19:51:14 +0530

    Feature# Make navbar static

--- src/components/internal/layout/MarketingHeader.jsx
+++ src/components/internal/layout/MarketingHeader.jsx
@@ -6,33 +6,6 @@ const MarketingHeader = ({ content }) => {
   const [isVisible, setIsVisible] = useState(true);
   const location = useLocation();
 
-  useEffect(() => {
-    let timer;
-
-    const handleMouseMove = () => {
-      setIsVisible(true);
-      clearTimeout(timer);
-      timer = setTimeout(() => setIsVisible(false), 2000);
-    };
-
-    const handleMouseIdle = () => {
-      clearTimeout(timer);
-      setIsVisible(false);
-    };
-
-    const timeout = setTimeout(handleMouseIdle, 2000);
-
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseleave", handleMouseIdle);
-
-    return () => {
-      clearTimeout(timeout);
-      clearTimeout(timer);
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseleave", handleMouseIdle);
-    };
-  }, []);
-
   const getButtonClassName = (hash) => {
     return location.hash === hash ? "button button-primary" : "button";
   };
